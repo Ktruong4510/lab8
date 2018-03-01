587115ab09dd4669553be6ff20454c33b88f6bd0
Kenneth Truong <ket032@ucsd.edu>
2018-03-01 01:23:20 -0800

    thirdpartyAPI lab

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,16 @@
 function initMap() {
 	// add your code here
+	var ucsd_ltlng = {lat:32.8794363, lng:-117.2371568};
+
+	// Create a map object and specify the DOM element for display.
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: ucsd_ltlng,
+		zoom: 15
+	});
+
+	var marker = new google.maps.Marker({
+		position: ucsd_ltlng,
+		map:map,
+		title: "Student Services Center"
+	}); 	
 }
